61eb7b70843a28615960b89b094620329d33f5cd
AtanaelMendes <atanael_snp@hotmail.com>
2022-04-21 17:21:10 -0300

    antes de mudar pro liunux

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 use \Slim\Slim;
 use \Rootdir\PageController;
+use \Rootdir\PageAdminController;
 
 require_once("vendor/autoload.php");
 
@@ -9,9 +10,17 @@ $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
+// $app->get('/', function() {
     
-	$page = new PageController;
+// 	$page = new PageController;
+
+// 	$page->setTpl("index");
+	
+// });
+
+$app->get('/gestao-login', function() {
+    
+	$page = new PageAdminController;
 
 	$page->setTpl("index");
 	
---
+++ vendor/project/php-classes/src/PageAdminController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Rootdir;
+
+class PageAdminController extends PageController {
+
+    public function __construct($opts = [], $tpl_dir = "/views/owner/") {
+
+        parent::__construct($opts, $tpl_dir);
+
+    }
+
+}
